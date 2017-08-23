30f9192ac46bde1cf12a1bf5b22a325f5d5900e6
Morris Jones <mojo@whiteoaks.com>
2017-08-23 03:30:50 +0000

    I finally got the /clubs endpoint to output the list from nap/qual
    Next up, make it a real template, and embed the output with <pre> or
    with HTML formatting or that kind of stuff.

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import bottle
 
+import sys, os
 from bottle import route, template
+import StringIO
+from nap import nap
+
+__cwd__ = os.path.dirname(os.path.realpath(__file__))
 
 @route('/')
 def index():
   return template('<h1>{{message}}</h1>', message="North American Pairs")
+
+@route('/clubs')
+def clubs():
+  output = nap.main(__cwd__,["-c"])
+  return output
+  
 
 if __name__ == '__main__':
   bottle.run(host='0.0.0.0', port=8080)
