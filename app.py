eca97db577af2cf0a73b0706cb525b6f169e8984
Morris Jones <mojo@whiteoaks.com>
2017-08-23 05:08:35 +0000

    Move gamefiles to a different directory

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import StringIO
 from nap import nap
 
 __cwd__ = os.path.dirname(os.path.realpath(__file__))
+gamefile_tree = os.environ['GAMEFILE_TREE']
 
 @route('/')
 def index():
@@ -13,12 +14,12 @@ def index():
 
 @route('/clubgames')
 def clubs():
-  output = nap.main(__cwd__,["-c"])
+  output = nap.main(__cwd__,["-t",gamefile_tree,"-c"])
   return template('report',title='Qualifier games reported',report=output)
 
 @route('/summary')
 def clubs():
-  output = nap.main(__cwd__,["-s"])
+  output = nap.main(__cwd__,["-t",gamefile_tree,"-s"])
   return template('report',title='Summary of all qualifiers',report=output)
 
 
