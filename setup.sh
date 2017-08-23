38d01e0c8b6480cf8b225d3d2626dca0745ce79c
Morris Jones <mojo@whiteoaks.com>
2017-08-23 05:09:39 +0000

    Add a note for setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ virtualenv --no-site-packages --distribute .env && \
   source .env/bin/activate && \
   pip install -r requirements.txt
 
+echo "Set GAMEFILE_TREE to the top of the gamefile dir tree"
